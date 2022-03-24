43e669072d46f776008237a51d9d979bf458669e
mzbasler <basler.mauricio@gmail.com>
2022-03-24 00:52:32 -0300

    configuração do formulário php

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,25 +1,25 @@
 <?php
 
-if (isset($_POST['email']) && !empty($_POST['email'])) {
+  if (isset($_POST['email']) && !empty($_POST['email'])){
 
-  $nome = addslashes($_POST['name']);
-  $email = addslashes($_POST['email']);
-  $mensagem = addslashes($_POST['message']);
+    $nome = addslashes($_POST['name']);
+    $email = addslashes($_POST['email']);
+    $mensagem = addslashes($_POST['message']);
 
-  $to = "basler.mauricio@gmail.com";
-  $subject = "Agendamento de Sessão";
-  $body = "Nome: " . $nome . " \r\n" .
-    "Email: " . $email . " \r\n" .
-    "Mensagem: " . $mensagem;
-  $header = "From: mzbasler@outlook.com" . " \r\n" .
-    "Reply-To:" . $email . " \r\n" .
-    "X=Mailer:PHP/" . phpversion();
+    $to = "basler.mauricio@gmail.com";
+    $subject = "Agendamento de Sessão";
+    $body = "Nome: " . $nome . " \r\n" .
+      "Email: " . $email . " \r\n" .
+      "Mensagem: " . $mensagem;
+    $header = "From: mzbasler@outlook.com" . " \r\n" .
+      "Reply-To:" . $email . " \r\n" .
+      "X=Mailer:PHP/" . phpversion();
 
-  if (mail($to, $subject, $body, $header)) {
+    if (mail($to, $subject, $body, $header)) {
 
-    echo ("Email enviado");
-  } else {
+      echo ("Email enviado");
+    } else {
 
-    echo ("Email não pode ser enviado");
+      echo ("Email não pode ser enviado");
+    }
   }
-}
